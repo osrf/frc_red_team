8051f3aa71692eb9e93e13deb5aeb07a9cb0aa30
Steve Peters <scpeters@osrfoundation.org>
2016-05-06 12:30:51 -0700

    Rename plugin class name to prevent duplication

---
+++ frcred_gazebo/src/joy_to_joints/JoyToJointsCanon.hh
@@ -0,0 +1,44 @@
+#include <thread>
+#include <gazebo_plugins/gazebo_ros_utils.h>
+
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <sensor_msgs/Joy.h>
+
+#include "gazebo/common/Plugin.hh"
+
+namespace gazebo
+{
+  struct ButtonInfo
+  {
+    int button;
+    physics::JointPtr joint;
+    double scale;
+    std::string type;
+  };
+
+  /// \brief A plugin that simulates buoyancy of an object immersed in fluid.
+  class GAZEBO_VISIBLE JoyToJointsCanon : public ModelPlugin
+  {
+    /// \brief Constructor.
+    public: JoyToJointsCanon();
+    public: ~JoyToJointsCanon();
+
+    /// \brief Read the model SDF to compute volume and center of volume for
+    /// each link, and store those properties in volPropsMap.
+    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+
+    private: void OnJoy(const sensor_msgs::Joy::ConstPtr &_msg);
+
+    private: GazeboRosPtr gazeboRos;
+    private: ros::Subscriber joySub;
+
+    private: ros::CallbackQueue callbackQueue;
+    private: std::thread callbackQueueThread;
+    private: void QueueThread();
+
+    private: std::vector<ButtonInfo> buttons;
+    private: physics::ModelPtr model;
+    private: bool alive;
+  };
+}
